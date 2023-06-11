5c5f6d07dbe96d514f26adee8f51511e3ced4418
hostemail <45652715+hostemail@users.noreply.github.com>
2023-06-11 15:14:03 +0800

    Update ChatInput.tsx
    恢复bottom

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -379,7 +379,6 @@ export const ChatInput = ({
           )}
         </div>
       </div>
-      /*
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
           href="https://github.com/mckaywrigley/chatbot-ui"
@@ -394,7 +393,6 @@ export const ChatInput = ({
           "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
         )}
       </div>
-      */
     </div>
   );
 };
